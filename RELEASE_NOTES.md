67707a399ff044bbb809033c20aa7c8be6c92598
devboy <dominic.graefen@gmail.com>
2014-12-10 12:19:07 +0100

    Bump version to 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.0.2 - December 10 2014
+* Adds FSharp.Data and FAKE dependencies in NuGet package
+
 #### 0.0.1 - December 6 2014
 * Tasks for executeMethod and TestTools
 * Initial release
--- src/FAKE.Unity3D/AssemblyInfo.fs
+++ src/FAKE.Unity3D/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FAKE.Unity3D")>]
 [<assembly: AssemblyProductAttribute("FAKE.Unity3D")>]
 [<assembly: AssemblyDescriptionAttribute("Unity3D tasks for FAKE")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
